e249bbe64e5a21ea59e32c9c40799a4fd4553367
Nhyarlathotep <renaud.melet@epitech.eu>
2022-06-09 01:37:35 +0200

    Add Google OAuth connection button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:oauth2_client/access_token_response.dart';
+import 'dart:developer';
+import 'package:oauth2_client/oauth2_helper.dart';
+import 'package:oauth2_client/src/io_web_auth.dart';
+import 'package:oauth2_client/google_oauth2_client.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+import 'package:http/http.dart' as http;
+
+//Instantiate an OAuth2Client...
+GoogleOAuth2Client client = GoogleOAuth2Client(
+  customUriScheme: 'com.example.mecene',
+  redirectUri: 'com.example.mecene:/oauth2redirect',
+);
+
+const String clientId = '';
+const String backendHost = '';
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +24,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +39,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +46,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () async {
+                OAuth2Helper oauth2Helper = OAuth2Helper(
+                  client,
+                  grantType: OAuth2Helper.AUTHORIZATION_CODE,
+                  clientId: clientId,
+                  scopes: [
+                    'https://www.googleapis.com/auth/userinfo.email',
+                    'https://www.googleapis.com/auth/userinfo.profile'
+                  ],
+                  webAuthClient: IoWebAuth(),
+                  webAuthOpts: {'preferEphemeral': true},
+                );
+                oauth2Helper.getToken().then((AccessTokenResponse? response) {
+                  log(name: 'ID', response!.respMap['id_token']);
+                  if ((response.respMap['id_token'] as String).isNotEmpty) {
+                    http.post(Uri(scheme: 'http', host: backendHost, path: 'user', port: 8000), headers: {
+                      'Authorization': 'Bearer ${response.respMap['id_token']}'
+                    }).then((http.Response response) {
+                      log(name: 'Response', response.toString());
+                    }).catchError((error) => print(error));
+                  }
+                });
+              },
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
